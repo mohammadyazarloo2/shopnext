dd37ecb5b8c3abc1d7331dac325524a231816bc7
mohammadyazarloo2 <34187298+mohammadyazarloo2@users.noreply.github.com>
2022-01-28 17:11:25 +0330

    Update checkout.js

--- pages/checkout.js
+++ pages/checkout.js
@@ -453,7 +453,7 @@ function Checkout(props) {
           <Grid item md={8}>
             <Card className={classes.p1}>
               <form>
-                <Stepper activeStep={activeStep} alternativeLabel>
+                <Stepper style={{direction="ltr"}} activeStep={activeStep} alternativeLabel>
                   {steps.map((label) => (
                     <Step key={label}>
                       <StepLabel> {label} </StepLabel>
