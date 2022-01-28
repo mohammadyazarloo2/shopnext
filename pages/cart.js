2cf6eee511c618f4e11e903bd7616066d97d6496
mohammadyazarloo2 <34187298+mohammadyazarloo2@users.noreply.github.com>
2022-01-28 16:03:57 +0330

    Update cart.js

--- pages/cart.js
+++ pages/cart.js
@@ -126,7 +126,7 @@ function Cart(props) {
                     <ListItem>
                       <Grid container>
                         <Typography variant="h6">
-                          subtotal: {cart.data.subtotal.formatted_with_symbol}
+                          جمع کل: {cart.data.subtotal.formatted_with_symbol}
                         </Typography>
                       </Grid>
                     </ListItem>
